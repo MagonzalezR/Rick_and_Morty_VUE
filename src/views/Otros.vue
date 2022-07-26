04471fdeded82d14df9fd548f215235f8182ab6e
Magordini <miguel.g3255@gmail.com>
2022-07-26 00:10:53 -0500

    agregando info para acceder a api

---
+++ src/views/Otros.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+        <div v-for="(personaje, index) in personajes" :key="index">
+            {{personaje.name}} <br/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            personajes:[]
+        }
+    },
+    methods:{
+        getData(){
+            this.axios.get("https://rickandmortyapi.com/api/character").then((response) => {
+                this.personajes=response.data.results
+            })
+        }
+
+    },
+    mounted(){
+        this.getData()
+    }
+}
+</script>
